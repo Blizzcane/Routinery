3ce544823487a71f09eff51e2782629495680437
Blizzcane <blizzcane@gmail.com>
2022-08-16 22:30:11 -0500

    adding table data

--- pages/Routines.js
+++ pages/Routines.js
@@ -10,27 +10,42 @@ import {
 } from "@mui/material";
 
 const columns = [
-  { id: "sunday", label: "Sun", minWidth: 10 },
-  { id: "monday", label: "Mon", minWidth: 10 },
-  { id: "tuesday", label: "Tue", minWidth: 10 },
-  { id: "wednesday", label: "Wed", minWidth: 10 },
-  { id: "thursday", label: "Thu", minWidth: 10 },
-  { id: "friday", label: "Fri", minWidth: 10 },
-  { id: "saturday", label: "Sat", minWidth: 10 },
+  { id: "sunday", label: "S", minWidth: 10 },
+  { id: "monday", label: "M", minWidth: 10 },
+  { id: "tuesday", label: "T", minWidth: 10 },
+  { id: "wednesday", label: "W", minWidth: 10 },
+  { id: "thursday", label: "T", minWidth: 10 },
+  { id: "friday", label: "F", minWidth: 10 },
+  { id: "saturday", label: "S", minWidth: 10 },
 ];
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(
+  name,
+  sunday,
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday
+) {
+  return {
+    name,
+    sunday,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+  };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen", true, false, false, true, false, false, true),
+  createData("yoghurt", false, true, false, true, true, false, false),
+  createData("Froz", false, true, true, false, false, false, false),
+  createData("Frozsdfa", true, false, false, true, false, true, false),
 ];
 
 const Routines = () => {
@@ -39,11 +54,12 @@ const Routines = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Habit</TableCell>
+            {columns.map((column) => (
+              <TableCell key={column.id} align="center">
+                {column.label}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
@@ -55,10 +71,13 @@ const Routines = () => {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="center">{row.sunday}</TableCell>
+              <TableCell align="center">{row.monday}</TableCell>
+              <TableCell align="center">{row.tuesday}</TableCell>
+              <TableCell align="center">{row.wednesday}</TableCell>
+              <TableCell align="center">{row.thursday}</TableCell>
+              <TableCell align="center">{row.friday}</TableCell>
+              <TableCell align="center">{row.saturday}</TableCell>
             </TableRow>
           ))}
         </TableBody>
