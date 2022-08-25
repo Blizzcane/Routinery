7a8d6c95f225d9c2f326105e8de40e2daf8bb9b9
Blizzcane <blizzcane@gmail.com>
2022-08-24 22:27:21 -0500

    typos

--- pages/Routines.js
+++ pages/Routines.js
@@ -18,35 +18,7 @@ const columns = [
   { id: "thursday", label: "T", minWidth: 10 },
   { id: "friday", label: "F", minWidth: 10 },
   { id: "saturday", label: "S", minWidth: 10 },
-];
-
-function createData(
-  name,
-  sunday,
-  monday,
-  tuesday,
-  wednesday,
-  thursday,
-  friday,
-  saturday
-) {
-  return {
-    name,
-    sunday,
-    monday,
-    tuesday,
-    wednesday,
-    thursday,
-    friday,
-    saturday,
-  };
-}
-
-const rows = [
-  createData("Frozen", true, false, false, true, false, false, true),
-  createData("yoghurt", false, true, false, true, true, false, false),
-  createData("Froz", false, true, true, false, false, false, false),
-];
+]; 
 
 const Routines = () => {
   const [routines, setRoutines] = useState([
